bd2e1ec9149768e38951a68d476196f5622ac4c2
vaitheeswari <vaitheeswari44@gmail.com>
2016-07-28 17:25:32 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Number
+{
+           public static void main(String[] arg)
+{
+
+   Scanner in=new Scanner(System in);
+      int a;
+      System.out.println("enter the numbers:");
+     if(a>=0)
+      System.out.println("the given number is positive");
+   else(a<0)
+      System.out.println("the given number is negative");
+}
+}
