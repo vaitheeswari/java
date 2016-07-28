792e1f2d450362163a938f8e349dbc028255cfda
vaitheeswari <vaitheeswari44@gmail.com>
2016-07-28 17:29:24 +0530

    Update Number.java

--- Number.java
+++ Number.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 class Number
 {
            public static void main(String[] arg)
-{
-
-   Scanner in=new Scanner(System in);
-      int a;
-      System.out.println("enter the numbers:");
-     if(a>=0)
-      System.out.println("the given number is positive");
-   else(a<0)
-      System.out.println("the given number is negative");
-}
+           {
+           int a;
+           Scanner in=new Scanner(System in);
+      
+           a=in.nextInt();
+            System.out.println("enter the numbers:");
+            if(a>=0)
+            System.out.println(n + "number is positive");
+            else(a<0)
+           System.out.println(n + "number is negative");
+           }
 }
