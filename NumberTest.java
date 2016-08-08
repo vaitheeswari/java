aa53e49f957dc66c5cc89859c5d646056503b79f
vaitheeswari <vaitheeswari44@gmail.com>
2016-08-08 06:25:31 -0700

    Add files via upload

---
+++ NumberTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Number
+{
+public static void main(String[],arg)
+{
+int b,n;
+Scanner in=new Scanner(System.in);
+b=in.nextInt();
+if(l>10&&l<50)
+{
+	if(a%2==0)
+
+System.out.println(b+"is even number");
+else
+
+System.out.println(b+"is not even number");
+}
+}
+}
